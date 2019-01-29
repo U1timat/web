a9b0ac27ef87186a0db10b6078fecc37411a2282
U1timat <39967298+U1timat@users.noreply.github.com>
2019-01-29 19:44:31 +0500

    Комменты страницы О магазине

--- about.php
+++ about.php
@@ -1,19 +1,19 @@
 ﻿<?php
-echo "<html>
+echo "<html> // собственно вывод строк
+<center> //выравнивает содержимое по центру
+<font face='Monotype Corsiva' size='15px'> //задание используемого шрифта и его размера
+<head>Кондитерская</head> //собственно заголовок окна страницы
+</font> //закрытие тега шрифта
+</center> //закрытие тега выравнивания по середине
+<link href='Style.css' rel='stylesheet' /> //установка связи с таблицей стилей
+<body> //открытие тега заполнения самой веб страницы
+<form> //открывем форму т.к. будет использоваться "табличный дизайн" (слева меню, справа основная информация страницы)
 <center>
-<font face='Monotype Corsiva' size='15px'>
-<head>Кондитерская</head>
-</font>
-</center>
-<link href='Style.css' rel='stylesheet' />
-<body>
-<form>
-<center>
-<table border='0' background='41.jpg'>
+<table border='0' background='41.jpg'> //заполняем фон  указанным файлом
 <tr align='center'>
-<td width='250px'>
-<ul>
-<li><font face='Monotype Corsiva' size='5px'><a href='index.php'>Главная</a></font></li>
+<td width='250px'> //размер в пикселях левой колонки (т.н. меню)
+<ul> //открываем маркированный список
+<li><font face='Monotype Corsiva' size='5px'><a href='index.php'>Главная</a></font></li> //задаем шрифт, размер шрифта, ссылку на страницу и название страницы
 <li><font face='Monotype Corsiva' size='5px'><a href='inet.php'>Интенет-магазин</a></font></li>
 <li><font face='Monotype Corsiva' size='5px'><a href='corzina.php'>Корзина</a></font></li>
 <li><font face='Monotype Corsiva' size='5px'><a href='shops.php'>Магазины в городе</a></font></li>
@@ -21,14 +21,14 @@ echo "<html>
 </ul>
 </font>
 </td>
-<td  width='800px' align='center'>
-<font face='Monotype Corsiva' size='10px'>
-<head>Прототип сайта создан ЭВТ-16-1БЗУ Горожаниновым Е.А. и Андроновым М.Д.</head>
+<td  width='800px' align='center'> //размер в пикселях правой части колонки (основное содержание страницы)
+<font face='Monotype Corsiva' size='10px'> //шрифт, размер
+<head>Прототип сайта создан ЭВТ-16-1БЗУ Горожаниновым Е.А. и Андроновым М.Д.</head> //основной текст страницы
 </font>
 </td>
 </tr>
 </table>
-<center>
+</center>
 </form>
 </body>
 </html>";
