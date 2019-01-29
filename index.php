bf594cca7c2e307a47dbe323188bb7b49728c862
U1timat <39967298+U1timat@users.noreply.github.com>
2019-01-29 20:03:24 +0500

    Коммент index и генератора идентификатора

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
 ﻿<?php
-session_start();
-echo "<html>
+session_start(); //создает новую сессию, или возобновляет старую
+echo "<html> //вывод текста
+<center> //выравнивает содержимое по центру
+<font face='Monotype Corsiva' size='15px'> //задание используемого шрифта и его размера
+<head>Кондитерская</head> //собственно заголовок окна страницы
+</font> //закрытие тега шрифта
+</center> //закрытие тега выравнивания по середине
+<link href='Style.css' rel='stylesheet' /> //установка связи с таблицей стилей
+<body> //открытие тега заполнения самой веб страницы
+<form> //открывем форму т.к. будет использоваться "табличный дизайн" (слева меню, справа основная информация страницы)
 <center>
-<font face='Monotype Corsiva' size='15px'>
-<head>Кондитерская</head>
-</font>
-</center>
-<link href='Style.css' rel='stylesheet' />
-<body>
-<form>
-<center>
-<table border='0' background='41.jpg'>
-<tr>
-<td width='250px'>
-<ul>
-<li><font face='Monotype Corsiva' size='5px'><a href='index.php'>Главная</a></font></li>
+<table border='0' background='41.jpg'> //заполняем фон  указанным файлом
+<tr> //контейнер для создания строки таблицы
+<td width='250px'> //размер в пикселях левой колонки (т.н. меню)
+<ul> //открываем маркированный список
+<li><font face='Monotype Corsiva' size='5px'><a href='index.php'>Главная</a></font></li> //задаем шрифт, размер шрифта, ссылку на страницу и название страницы
 <li><font face='Monotype Corsiva' size='5px'><a href='inet.php'>Интенет-магазин</a></font></li>
 <li><font face='Monotype Corsiva' size='5px'><a href='corzina.php'>Корзина</a></font></li>
 <li><font face='Monotype Corsiva' size='5px'><a href='shops.php'>Магазины в городе</a></font></li>
 <li><font face='Monotype Corsiva' size='5px'><a href='about.php'>О компании</a></font></li>
 </ul>
-</font>
 </td>
-<td  width='800px' align='center'>
+<td  width='800px' align='center'> //размер в пикселях правой части колонки (основное содержание страницы)
 <li><font face='Monotype Corsiva' size='10px'>Вкусные пирожки и тортики<br/>Начинка на любой вкус и кошелёк...<br/>Приходите: будем рады!</font></li>
 </td>
 </tr>
--- repository.php
+++ repository.php
@@ -1,15 +1,15 @@
 <?php
 
-function GUID()
+function GUID() //инициализируем использование функции GUID (уникальный идентификатор каждого пользователя, для работы с корзиной и заказами) 
 {
-    if (function_exists('com_create_guid') === true)
+    if (function_exists('com_create_guid') === true) //собственно генерирует GUID
     {
-        return trim(com_create_guid(), '{}');
+        return trim(com_create_guid(), '{}'); //возвращает созданный идентификатор
     }
 
-    return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X',
+    return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', //возвращает значение, полученное с помощью 4 генераторов
 
-        mt_rand(0, 65535), mt_rand(0, 65535),
+        mt_rand(0, 65535), mt_rand(0, 65535), 
         mt_rand(0, 65535), mt_rand(16384, 20479),
         mt_rand(32768, 49151), mt_rand(0, 65535),
         mt_rand(0, 65535), mt_rand(0, 65535));
