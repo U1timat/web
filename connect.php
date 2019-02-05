4c3ad17cc4b7efdb1ced6ab23a6e349d816ae3f6
U1timat <39967298+U1timat@users.noreply.github.com>
2019-02-05 12:31:18 +0500

    connect real_escape

--- connect.php
+++ connect.php
@@ -3,6 +3,8 @@ ini_set('display_errors','Off');
 mysql_connect("92.243.95.80","candyshop","dEpd66_5") //устанавливаем соединение по указанному адресу, с указанным логином и паролем
        or die("MySQL недоступна!".mysql_error()); // в случае отрицательного исхода событий выдаем ошибку
 mysql_select_db("candyshop") or die("Нет соединения с БД!".mysql_error()); //выбираем нужную базу, если не получается по какой-либо причине (нет базы или доступа) пишем ошибку
-mysql_query("set names 'utf8'"); //создаем запрос, при котором задаем используемую кодировку при работе с базой
 
+mysql_query("set names 'utf8'"); //создаем запрос, при котором задаем используемую кодировку при работе с базой
+	mysql_real_escape_string($sqluser),
+	mysql_real_escape_string($sqlpassword));
 ?>
