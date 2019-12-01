0ee24e2aa1ac30b53580ac7b4965fbcdfcc4675f
U1timat <39967298+U1timat@users.noreply.github.com>
2019-12-01 23:22:37 +0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,12 @@
 <?php
-ini_set('display_errors','Off');
-mysql_connect("92.243.95.80","candyshop","dEpd66_5") //устанавливаем соединение по указанному адресу, с указанным логином и паролем
-       or die("MySQL недоступна!".mysql_error()); // в случае отрицательного исхода событий выдаем ошибку
-mysql_select_db("candyshop") or die("Нет соединения с БД!".mysql_error()); //выбираем нужную базу, если не получается по какой-либо причине (нет базы или доступа) пишем ошибку
+require_once 'd:/shop/access/conf_acc.php'
+
+$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_errno()) {
+    printf("ошибка подключения: %s\n", mysqli_connect_error());
+    exit();
+}
 
 mysql_query("set names 'utf8'"); //создаем запрос, при котором задаем используемую кодировку при работе с базой
 	mysql_real_escape_string($sqluser),
